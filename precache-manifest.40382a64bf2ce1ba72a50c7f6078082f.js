be0c1c4f41b18085fb4ae4dec301c6c8c9f16ec7
Trugamr <6trugamr9@gmail.com>
2020-03-15 11:22:56 +0530

    Updates

---
+++ precache-manifest.40382a64bf2ce1ba72a50c7f6078082f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3948127e86e7b2ee384f807d3e379b3",
+    "url": "/playground/index.html"
+  },
+  {
+    "revision": "0e84f8734ad68c26e8b8",
+    "url": "/playground/static/css/main.182768f0.chunk.css"
+  },
+  {
+    "revision": "91ec6bd5fccfb09fead3",
+    "url": "/playground/static/js/2.0c273159.chunk.js"
+  },
+  {
+    "revision": "0e84f8734ad68c26e8b8",
+    "url": "/playground/static/js/main.4a09eece.chunk.js"
+  },
+  {
+    "revision": "49effa767ea8d7bd1610",
+    "url": "/playground/static/js/runtime~main.c80ac52c.js"
+  }
+]);
